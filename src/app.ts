0a8b928e8ebc09b070404be582084c6fe700bdbb
thiagoconsult <thiago.consultx@gmail.com>
2024-07-27 20:16:59 -0300

    fix(main): update `app.ts`

--- src/app.ts
+++ src/app.ts
@@ -18,35 +18,6 @@ app.register(cors, {
   methods: "*",
 });
 
-// app.register(swagger, {
-//   openapi: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "BLOGGING",
-//       description: "Tech Challenge 2 - Grupo 8",
-//       version: "0.1.0",
-//     },
-//     servers: [
-//       {
-//         url: "https://tech-challenge2.grupo8.tech/",
-//         description: "Production",
-//       },
-//       {
-//         url: "http://localhost:3000/",
-//         description: "Development",
-//       },
-//     ],
-//     components: {
-//       securitySchemes: {
-//         bearerAuth: {
-//           type: "http",
-//           scheme: "bearer",
-//         },
-//       },
-//     },
-//   },
-// });
-
 app.register(swagger, swaggerConfig);
 
 app.register(swaggerUi, {
