8644ed0f39f2497b9140779c6c39ae63085b0e44
lucasmoraist <luksmnt1101@gmail.com>
2024-09-30 11:38:34 -0300

    refactor: Add getTeacherById method to TeacherRepository

--- src/repositories/pg/teacher.repository.ts
+++ src/repositories/pg/teacher.repository.ts
@@ -1,6 +1,7 @@
 import { Teacher } from "@/entities/teacher.entity";
 import { ITeacherRepository } from "@/repositories/teacher.repository.interface";
 import { database } from "@/lib/pg/db";
+import { Post } from "@/entities/post.entity";
 
 export class TeacherRepository implements ITeacherRepository {
   async create({ name, school_subject, user_id }: Teacher): Promise<Teacher> {
@@ -14,16 +15,36 @@ export class TeacherRepository implements ITeacherRepository {
     return result?.rows[0];
   }
 
-  async getName(id: number): Promise<string> {
+  async getTeacherByUserId(userId: number): Promise<number> {
     const result = await database.clientInstance?.query(
       `
-      SELECT name FROM teacher WHERE teacher.user_id = $1
+        SELECT t.id FROM teacher t WHERE t.user_id = $1
       `,
-      [id]
+      [userId]
     );
 
-    const name = result?.rows[0].name;
-
-    return name;
+    return result?.rows[0].id;
   }
+
+  async getTeacherById(teacherId: number): Promise<Teacher & { posts: Post[] }> {
+    const teacherResult = await database.clientInstance?.query(`
+      SELECT * FROM teacher WHERE id = $1
+    `, [teacherId]);
+  
+    const teacher = teacherResult?.rows[0];
+  
+    if (!teacher) {
+      throw new Error('Teacher not found');
+    }
+  
+    const postsResult = await database.clientInstance?.query(`
+      SELECT * FROM post WHERE teacher_id = $1
+    `, [teacherId]);
+  
+    const posts = postsResult?.rows || [];
+  
+    // Adiciona os posts diretamente no objeto do professor
+    return { ...teacher, posts };
+  }
+  
 }
