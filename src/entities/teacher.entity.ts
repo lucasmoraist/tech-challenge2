051c7511904243ccec34895768d253be031e08ec
lucasmoraist <luksmnt1101@gmail.com>
2024-09-30 10:12:00 -0300

    Update user and teacher entities

--- src/entities/teacher.entity.ts
+++ src/entities/teacher.entity.ts
@@ -1,6 +1,9 @@
+import { Post } from "./post.entity";
+
 export interface Teacher {
   id?: number | undefined;
   name: string;
   school_subject: string;
   user_id?: number | undefined;
+  posts: Post[] | [];
 }
