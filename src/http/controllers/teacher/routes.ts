3ac7234ad7523b4e8ad3ca427d904040503a19eb
lucasmoraist <luksmnt1101@gmail.com>
2024-10-02 15:23:33 -0300

    fix: concluing

--- src/http/controllers/teacher/routes.ts
+++ src/http/controllers/teacher/routes.ts
@@ -4,8 +4,10 @@ import { createTeacherSchema } from "@/lib/helper/swagger/teacher/create-teacher
 import { getOne } from "./get-one";
 import { jwtValidate } from "@/http/middlewares/jwt-validate";
 
+// TODO: Adicionar requisições de atualização, listagem completa e exlusão de professores
 export async function teacherRoutes(app: FastifyInstance) {
   app.post("/teacher", createTeacherSchema, create);
+  // TODO: Adicionar schema do swagger
   app.get("/admin/teacher/:teacherId", 
     { onRequest: [jwtValidate] }, 
     getOne);
