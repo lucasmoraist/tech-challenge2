94b36f8b3c278b2229f6169f3c382fed7273c4bf
lucasmoraist <luksmnt1101@gmail.com>
2024-10-02 15:22:18 -0300

    refactor: Update teacher route to require JWT authentication for admin access

--- src/http/controllers/teacher/routes.ts
+++ src/http/controllers/teacher/routes.ts
@@ -2,8 +2,11 @@ import { FastifyInstance } from "fastify";
 import { create } from "./create";
 import { createTeacherSchema } from "@/lib/helper/swagger/teacher/create-teacher-schema";
 import { getOne } from "./get-one";
+import { jwtValidate } from "@/http/middlewares/jwt-validate";
 
 export async function teacherRoutes(app: FastifyInstance) {
   app.post("/teacher", createTeacherSchema, create);
-  app.get("/teacher/:teacherId", getOne);
+  app.get("/admin/teacher/:teacherId", 
+    { onRequest: [jwtValidate] }, 
+    getOne);
 }
