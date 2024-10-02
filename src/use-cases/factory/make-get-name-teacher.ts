11de27c9ee2cccc759ee64fa719a624375612b59
lucasmoraist <luksmnt1101@gmail.com>
2024-10-02 13:51:46 -0300

    refactor: Remove unused makeGetIdTeacherUseCase function

--- src/use-cases/factory/make-get-name-teacher.ts
+++
@@ -1,9 +0,0 @@
-import { TeacherRepository } from "@/repositories/pg/teacher.repository";
-import { GetIdTeacherUseCase } from "../get-teacher-user-id";
-
-export function makeGetIdTeacherUseCase() {
-  const teacherRepository = new TeacherRepository();
-  const getIdTeacherUseCase = new GetIdTeacherUseCase(teacherRepository);
-
-  return getIdTeacherUseCase;
-}
---
+++ src/use-cases/factory/make-get-teacher-user-id.ts
@@ -0,0 +1,8 @@
+import { TeacherRepository } from "@/repositories/pg/teacher.repository";
+import { GetTeacherByUserIdUseCase } from "../get-teacher-user-id";
+
+export function makeGetTeacherUserIdUseCase() {
+    const teacherRepository = new TeacherRepository();
+    const getIdTeacherUseCase = new GetTeacherByUserIdUseCase(teacherRepository);
+    return getIdTeacherUseCase;
+}
