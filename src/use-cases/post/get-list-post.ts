6d0ba4b03212b3c1a453fe94e3475aa1e40f3396
lucasmoraist <luksmnt1101@gmail.com>
2024-11-28 13:47:30 -0300

    refactor: rename method to getAll in GetListPostUseCase

--- src/use-cases/post/get-list-post.ts
+++ src/use-cases/post/get-list-post.ts
@@ -6,6 +6,6 @@ export class GetListPostUseCase {
   constructor(private readonly postRepository: IPostRepository) {}
 
   async handler(page: number, limit: number): Promise<postSummary> {
-    return this.postRepository.getList(page, limit);
+    return this.postRepository.getAll(page, limit);
   }
 }
